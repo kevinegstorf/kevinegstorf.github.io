50f6c574e20a59279c35fbc19a866459eab8c046
kevinegstorf <kevin.egstorf@gmail.com>
2017-06-26 20:45:00 +0200

    added bootstrap and vue

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ let paragraph = document.createElement('p');
 paragraph.textContent = 'Welcome to my site'
 
 myHeader.appendChild(paragraph)
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
