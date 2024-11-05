020ad765f8fee0ee7c8e93dbd44eaa9d1d7ac5aa
Kevin Egstorf <kevin.egstorf@gmail.com>
2024-11-05 15:56:34 +0100

    updated content

--- index.js
+++ index.js
@@ -59,18 +59,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  const cookieConsent = document.getElementById('cookie-consent');
-  const acceptCookies = document.getElementById('accept-cookies');
-
-  if (!localStorage.getItem('cookiesAccepted')) {
-    cookieConsent.style.display = 'block';
-  }
-
-  acceptCookies.addEventListener('click', function() {
-    localStorage.setItem('cookiesAccepted', 'true');
-    cookieConsent.style.display = 'none';
-  });
-});
